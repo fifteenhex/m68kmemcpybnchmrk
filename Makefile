678d3fd864e8c6703e53762c53b4de3de8bd0398
Daniel Palmer <daniel@0x0f.com>
2025-05-02 12:47:28 +0900

    Actually works now :D

--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
 CC=m68k-linux-gnu-gcc
-KDIR=../m68k_nolibc/linux/
+KDIR=../m68k_nolibc/linux
 
 m68kmemcpybnchmrk: main.c
-	$(CC) -m68040 -nostdlib -static -include $(KDIR)/tools/include/nolibc/nolibc.h -Os -o $@ $< -lgcc
+	$(CC) -ffunction-sections -Wl,-gc-sections -nostdlib -static -m68040 \
+		-include $(KDIR)/tools/include/nolibc/nolibc.h -O3 -o $@ $< -lgcc
--- main.c
+++ main.c
@@ -1,6 +1,9 @@
+#define XXH_INLINE_ALL
+#include "xxhash.h"
+
 #define SZ_1K 1024
 #define SZ_1M (SZ_1K * 1024)
-#define TEST_SZ (SZ_1M * 256)
+#define TEST_SZ (SZ_1M * 16)
 #define COPY_SZ (128 * SZ_1K)
 #define BUFF_SZ (COPY_SZ * 2)
 
@@ -9,14 +12,19 @@
 #define ARRAY_SZ(a) (sizeof(a) / sizeof(a[0]))
 #define ALIGN_PAD 16
 
-static long get_us(void)
+static long get_us(const struct timeval *tv)
 {
-	struct timeval tv;
-
-	sys_gettimeofday(&tv, NULL);
-
-	return (tv.tv_sec * (1000000l)) + tv.tv_usec;
+	return (tv->tv_sec * (1000000l)) + tv->tv_usec;
 }
+
+/* libc */
+static void memcpy_memcpy(void *to, const void *from)
+{
+	for (int j = 0; j < OUTTER_LOOPS; j++) {
+		memcpy(to, from, COPY_SZ);
+	}
+}
+
 
 /* Dumb */
 
@@ -56,6 +64,40 @@ static void memcpy_dumb_longs(void *to, const void *from)
 	}
 }
 
+static void memcpy_4_longs(void *to, const void *from)
+{
+	for (int j = 0; j < OUTTER_LOOPS; j++) {
+		for (int i = 0; i < COPY_SZ; i += (4 * 4)) {
+			const uint32_t *lfrom = (from + i);
+			uint32_t *lto = (to + i);
+
+			*lto++ = *lfrom++;
+			*lto++ = *lfrom++;
+			*lto++ = *lfrom++;
+			*lto++ = *lfrom++;
+		}
+	}
+}
+
+static void memcpy_8_longs(void *to, const void *from)
+{
+	for (int j = 0; j < OUTTER_LOOPS; j++) {
+		for (int i = 0; i < COPY_SZ; i += (4 * 8)) {
+			const uint32_t *lfrom = (from + i);
+			uint32_t *lto = (to + i);
+
+			*lto++ = *lfrom++;
+			*lto++ = *lfrom++;
+			*lto++ = *lfrom++;
+			*lto++ = *lfrom++;
+			*lto++ = *lfrom++;
+			*lto++ = *lfrom++;
+			*lto++ = *lfrom++;
+			*lto++ = *lfrom++;
+		}
+	}
+}
+
 /* movem */
 
 static inline void movem_copy32(void *dest, const void *src)
@@ -84,8 +126,7 @@ static inline void move16_copy32(void *dest, const void *src)
 {
 	asm volatile ("move16 (%[from])+, (%[to])+\n"
 				  "move16 (%[from])+, (%[to])+\n"
-				  :
-				  : [from] "a" (src), [to] "a" (dest));
+				  : [from] "+a" (src), [to] "+a" (dest));
 }
 
 static inline void move16_copy64(void *dest, const void *src)
@@ -94,8 +135,7 @@ static inline void move16_copy64(void *dest, const void *src)
 				  "move16 (%[from])+, (%[to])+\n"
 				  "move16 (%[from])+, (%[to])+\n"
 				  "move16 (%[from])+, (%[to])+\n"
-				  :
-				  : [from] "a" (src), [to] "a" (dest));
+				  : [from] "+a" (src), [to] "+a" (dest));
 }
 
 static inline void move16_copy128(void *dest, const void *src)
@@ -108,8 +148,7 @@ static inline void move16_copy128(void *dest, const void *src)
 				  "move16 (%[from])+, (%[to])+\n"
 				  "move16 (%[from])+, (%[to])+\n"
 				  "move16 (%[from])+, (%[to])+\n"
-				  :
-				  : [from] "a" (src), [to] "a" (dest));
+				  : [from] "+a" (src), [to] "+a" (dest));
 }
 
 static void memcpy_move16_32(void *to, const void *from)
@@ -154,9 +193,12 @@ struct memcpy_impl {
 };
 
 static const struct memcpy_impl impls[] = {
+	{ .name = "memcpy", .func = memcpy_memcpy },
 	{ .name = "dumb_bytes", .func = memcpy_dumb_bytes },
 	{ .name = "dumb_words", .func = memcpy_dumb_words },
 	{ .name = "dumb_longs", .func = memcpy_dumb_longs },
+	{ .name = "4_longs", .func = memcpy_4_longs },
+	{ .name = "8_longs", .func = memcpy_8_longs },
 	{ .name = "movem_32", .func = memcpy_movem_32 },
 	{ .name = "move16_32", .func = memcpy_move16_32 },
 	{ .name = "move16_64", .func = memcpy_move16_64 },
@@ -184,13 +226,34 @@ int main(int argc, char **argv, char **envp)
 	for (int i = 0; i < ARRAY_SZ(impls); i++) {
 		const struct memcpy_impl *impl = &impls[i];
 
-		long start = get_us();
-		impls->func(to, from);
-		long end = get_us();
+		printf("Filling with garbage...");
+		getrandom(buffer, BUFF_SZ + ALIGN_PAD, 0);
+		uint32_t original_from_hash = XXH32(from, COPY_SZ, 0);
+		printf("done\n");
+
+		struct timeval start_tv = { 0 };
+		sys_gettimeofday(&start_tv, NULL);
+
+		impl->func(to, from);
+
+		struct timeval end_tv = { 0 };
+		sys_gettimeofday(&end_tv, NULL);
+
+		/* Make sure the hardwork for calculating the time happens after the test has happened */
+		long start = get_us(&start_tv);
+		long end = get_us(&end_tv);
 		long duration = end - start;
 
+		uint32_t from_hash = XXH32(from, COPY_SZ, 0);
+		uint32_t to_hash = XXH32(to, COPY_SZ, 0);
+
+		if ((original_from_hash != from_hash) || (to_hash != from_hash)) {
+			printf("xxhash32 original from: 0x%08x, from: 0x%08x, to 0x%08x\n",
+				original_from_hash, from_hash, to_hash);
+		}
+
 		//float speed = TEST_SZ / duration;
-		printf("%s\t%ld uS (xx MiB/s)\n", impl->name, duration);
+		printf("%s\t\t%ld uS (xx MiB/s)\n", impl->name, duration);
 	}
 
 	return 0;
